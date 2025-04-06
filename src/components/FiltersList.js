be3d9391459ad3fe335045b093f7ac6b6563262e
Irene <isjv87@gmail.com>
2025-04-06 15:03:51 +0200

    Continuing with the async/await update

--- src/components/FiltersList.js
+++ src/components/FiltersList.js
@@ -9,25 +9,32 @@ function FiltersList(props) {
 
 	// This useEffect brings the filters from the database with the fetch
 	useEffect(() => {
-		// Variable that has the url that is needed for the fetch
-		const url = `http://localhost:3001/filters?type=${props.filterName}`;
+		const fetchData = async () => {
+			try {
+				// Variable that has the url that is needed for the fetch
+				const url = `http://localhost:3001/filters?type=${props.filterName}`;
+				// Variable that saves the options that the fetch needs
+				const options = {
+					method: "GET",
+					mode: "cors",
+					headers: { "Content-Type": "application/json" },
+				};
 
-		// Variable that saves the options that the fetch needs
-		const options = {
-			method: "GET",
-			mode: "cors",
-			headers: { "Content-Type": "application/json" },
+				const response = await fetch(url, options);
+
+				if (!response.ok) {
+					throw new Error("HTTP error " + response.status);
+				}
+				const data = await response.json();
+				setArrayFilters(data);
+			} catch (error) {
+				console.log(error);
+			}
 		};
 
-		fetch(url, options)
-			.then((resp) => resp.json())
-			.then(function (data) {
-				setArrayFilters(data);
-			})
-			.catch(function (error) {
-				console.log(error);
-			});
-	}, [props]);
+		fetchData();
+
+	}, [props.filterName]);
 
 	return (
 		<ul className={styles.listInfo}>
--- src/components/LandingPageUserProfile.js
+++ src/components/LandingPageUserProfile.js
@@ -9,13 +9,10 @@ import { logout } from "../slicers/loginSlice";
 function LandingPageUserProfile(props) {
 	// Variable that we need to be able to use dispatchers
 	const logoutDispatch = useDispatch();
-
 	// This useSelector variable has the token that it is saved in the store
 	const { token } = useSelector((state) => state.loginReducer);
-
 	// Variable that has the url that is needed for the fetch
 	const url = "http://localhost:3001/login";
-
 	// Variable that saves the options that the fetch needs
 	const options = {
 		method: "DELETE",
@@ -26,19 +23,23 @@ function LandingPageUserProfile(props) {
 		},
 	};
 
-	// Function for logging out
-	const logOut = () => {
-		fetch(url, options)
-			.then((resp) => resp.json())
-			.then(function (data) {
-				// The user logs out...
-				logoutDispatch(logout());
-				// ...and his/her token is deleted
-				delete sessionStorage["token"];
-			})
-			.catch(function (error) {
-				console.log(error);
-			});
+	/** The function for loggin out is a DELETE request to http://localhost:3001/login. 
+	Typically, a DELETE request to a login endpoint is used to invalidate a session or token on the server-side. 
+	In many cases, the server might not send back any meaningful data in the response body for a successful DELETE 
+	request. It might just send back a status code (like 204 No Content) to indicate success.*/
+	const logOut = async () => {
+		try {
+			const response = await fetch(url, options);
+			if (!response.ok) {
+				throw new Error("HTTP error " + response.status);
+			}
+			// The user logs out...
+			logoutDispatch(logout());
+			// ...and his/her token is deleted
+			delete sessionStorage["token"];
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	return (
